27a8fc509d3da303d863c2bd6966ebf9b1c6d99b
brightspark3 <github@gop.id.au>
2026-05-23 18:07:39 +1000

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -195,10 +195,10 @@ configure_postfix() {
 
 	# Generate the EDH parameters
 	cd /etc/postfix
-	openssl dhparam -out dh512.tmp 512 && mv dh512.tmp dh512.pem
-	openssl dhparam -out dh1024.tmp 1024 && mv dh1024.tmp dh1024.pem
+	#openssl dhparam -out dh512.tmp 512 && mv dh512.tmp dh512.pem
+	#openssl dhparam -out dh1024.tmp 1024 && mv dh1024.tmp dh1024.pem
 	openssl dhparam -out dh2048.tmp 2048 && mv dh2048.tmp dh2048.pem
-	chmod 644 dh512.pem dh1024.pem dh2048.pem
+	chmod 644 dh2048.pem
 	cd $IMAGE_HOME
 
 	postmap /etc/postfix/without_ptr 
