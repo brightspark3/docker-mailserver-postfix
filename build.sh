89ab2c42c8a5937351dbd3d67706ab8917ab201f
brightspark3 <github@gop.id.au>
2026-05-23 18:17:43 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ docker buildx build \
      -t george0us/mailserver-postfix:latest \
      --push \
      --platform linux/amd64 \
+     . \
 
 #docker build --progress=plain -t george0us/mailserver-postfix:latest .
